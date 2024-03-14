dc4571338c43b2d7d6c988388312d50bc9615a04
Decaaaa <Avkb8237@gmail.com>
2024-03-14 13:03:03 -0700

    Reorganized and added waterfall

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float EnemyHealth;
+    public float damagetakenpers;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "playerContact") {
+            //Destroy(this.gameObject);
+            EnemyHealth-=damagetakenpers/30;
+        }
+    }
+}
