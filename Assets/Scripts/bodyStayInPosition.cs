9dfd39de7e2942b356a99f0d4293b2f92c6efc4e
aryamanrtunjay <aryamanrtunjay@gmail.com>
2024-06-23 23:46:24 -0700

    Fixed Jitter

---
+++ Assets/Scripts/bodyStayInPosition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bodyStayInPosition : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5f;
+    public float offset = 1.48f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float step = speed * Time.deltaTime;
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        Vector3 goal = new Vector3(target.position.x, target.position.y - offset, target.position.z);
+        transform.position = goal;
+    }
+}
